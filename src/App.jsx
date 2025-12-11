353fcd401cd1170f183c89cd83413a84a055e759
lwakluka <lwaklukasuyibam1@gmail.com>
2025-12-11 23:41:23 +0100

    fix: removed 'invert' class from App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import WeatherWidget from './components/WeatherWidget';
 function App() {
   return (
     <div
-      className="min-h-screen w-full invert  flex items-center justify-center p-4 relative overflow-hidden"
+      className="min-h-screen w-full flex items-center justify-center p-4 relative overflow-hidden"
       style={{
         backgroundImage:
           "url(https://images.unsplash.com/photo-1664295306817-092a7dacaeed?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
